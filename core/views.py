84967d851e106de3657bf7e42d5491dc5cb6f695
Ramiro <ramiro@gnubit.com>
2016-03-06 22:52:52 +0100

    -Re-organized version of bar's menus

--- core/views.py
+++ core/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.conf import settings as settings_file
+from core import views_utils as utils
 
 # Create your views here.
 
-def site_vars():
-	site_vars_data = {}
-	site_vars_data['name']=settings_file.SITE_NAME
-	site_vars_data['version']=settings_file.SITE_VERSION
-	return site_vars_data
-
 def index(request):
-	#return HttpResponse("<center>Dashboard place =>  Go to <a href=\"/tickets/\">tickets</a></center>")
-	return render(request, 'dashboard/index.html', {"site_vars": site_vars()})
+	return render(request, 'dashboard/index.html', {"site_vars": utils.site_vars()})
