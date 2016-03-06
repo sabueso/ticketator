bdd6e2674dae86affeb7946f543b700807a65bd3
Ramiro <ramiro@gnubit.com>
2016-03-06 21:57:01 +0100

    -Added a little function to collect all settings introduced via settings.py that we will want to use in render templates

--- core/views.py
+++ core/views.py
@@ -4,13 +4,12 @@ from django.conf import settings as settings_file
 
 # Create your views here.
 
-def site_name():
-	site_vars = {}
-	site_vars['name']=settings_file.SITE_NAME
-	site_vars['version']=settings_file.SITE_VERSION
-	return site_vars
+def site_vars():
+	site_vars_data = {}
+	site_vars_data['name']=settings_file.SITE_NAME
+	site_vars_data['version']=settings_file.SITE_VERSION
+	return site_vars_data
 
 def index(request):
 	#return HttpResponse("<center>Dashboard place =>  Go to <a href=\"/tickets/\">tickets</a></center>")
-	name = site_name()
-	return render(request, 'dashboard/index.html', {"site_name": site_name()})
+	return render(request, 'dashboard/index.html', {"site_vars": site_vars()})
