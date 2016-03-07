ba65954942372e240baaf1984d6482faac58f36f
Ramiro <ramiro@gnubit.com>
2016-03-07 19:14:08 +0100

    -Added a working version of "Add tickets"

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.forms import ModelForm
 
 #Added imports
 from django.contrib.auth.models import User
@@ -7,6 +8,10 @@ from datetime import datetime
 
 #TO-DO imports
 #from colorfield.fields import ColorField
+
+#Generic views...
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Create your models here.
 class Company(models.Model):
@@ -17,7 +22,7 @@ class Company(models.Model):
 		return self.name
 
 class Department(models.Model):
-        company_rel = models.ForeignKey('Company', on_delete=models.CASCADE )
+	company_rel = models.ForeignKey('Company', on_delete=models.CASCADE )
 	name = models.CharField(max_length=100)
 	#logo = pending....
 	#color  = if needed....
@@ -25,26 +30,24 @@ class Department(models.Model):
 		return self.name
 
 class Profile(models.Model):
-        user = models.OneToOneField(User)
-        department = models.ForeignKey('Department',on_delete=models.CASCADE )
-        notify_email =  models.BooleanField(default=False)
-        avatar =  models.FileField(upload_to='./avatar/')
-        #def __str__(self):
-        #    return self.id
+	user = models.OneToOneField(User)
+	department = models.ForeignKey('Department',on_delete=models.CASCADE )
+	notify_email =  models.BooleanField(default=False)
+	avatar =  models.FileField(upload_to='./avatar/')
 
+  
 class State(models.Model):
 	name = models.CharField(max_length=30)
 	description = models.CharField(max_length=130)
 	numvalue = models.IntegerField(null=True,blank=True,default=0)
 	active = models.BooleanField(default=True)
 	#color = ColorField()
-	
 	def __unicode__(self):
 		return self.name
 
 class Ticket(models.Model):
 	date = models.DateTimeField(default=datetime.now)
-	create_user = models.ForeignKey(User, related_name = "c_user")
+	create_user = models.ForeignKey(User, related_name = "c_user", blank=True, null=True,)
 	assigned_department = models.ForeignKey('Department', blank=True, null=True)
 	assigned_user = models.ForeignKey(User, blank=True, null=True, related_name = "a_user")
 	subject =  models.CharField(max_length=40)
@@ -53,3 +56,8 @@ class Ticket(models.Model):
 	
 	def __str__(self):
 		return '%s' % (self.id)
+
+class TicketForm(ModelForm):
+	class Meta:
+		model =  Ticket
+		fields = ['date','create_user','assigned_department','assigned_user','subject','body','assigned_state']
--- core/views_tickets.py
+++ core/views_tickets.py
@@ -1,8 +1,11 @@
 #Tickets views: list, create, delete
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from core import views_utils as utils
+from core.models import Ticket,TicketForm, State, Department
+from django.contrib.auth.models import User
+#Needed for forms
+from django.views.decorators.csrf import csrf_protect
 
 
 def list_tickets(request):
@@ -10,4 +13,19 @@ def list_tickets(request):
 
 
 def create_ticket(request):
-	return render(request, 'tickets/create_ticket.html', {"site_vars": utils.site_vars()})
+	#Status info
+	status_info = State.objects.filter()
+	dept_info = Department.objects.filter()
+	users_info = User.objects.filter()
+
+	#vars
+	site_vars = utils.site_vars()
+	status_info = status_info
+
+	if request.method == 'POST':
+		form = TicketForm(request.POST)
+		if form.is_valid():
+			form.save()
+	else:
+		form = TicketForm()
+	return render(request,'tickets/create_ticket.html', locals())
