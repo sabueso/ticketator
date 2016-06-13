8d9c45ffc5eabd4368da4755dcd4a563316c426d
Ramiro <ramiro@gnubit.com>
2016-06-13 06:27:07 +0200

    -Added full support for new queues

--- core/rights.py
+++ core/rights.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 import models
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 
 
 #Check the rights for user =>  ticket or for user =>  queue to bind this in actions
@@ -8,13 +9,29 @@ def get_rights_for_ticket(user, queue=None, ticket_id=None):
 	#If admin, we define can_edit, if not, we check it via query
 	class u_rights_obj(object):
 		def __init__(self):
+			def q_result(query_result):
+				if query_result ==  "all":
+					self.can_edit = True
+					self.can_view = True
+					self.can_create = True
+					self.can_delete = True
+					self.can_comment = True
+				elif query_result ==  None:
+					self.can_edit = False
+					self.can_view = False
+					self.can_create = False
+					self.can_delete = False
+					self.can_comment = False
+				else:
+					self.can_edit = r_obj.can_edit
+					self.can_view = r_obj.can_view
+					self.can_create = r_obj.can_create
+					self.can_delete = r_obj.can_delete
+					self.can_comment = r_obj.can_comment
+				return self
 			#Super-admin benefits :)
 			if user.username == 'admin':
-				self.can_edit = True
-				self.can_view = True
-				self.can_create = True
-				self.can_delete = True
-				self.can_comment = True
+				q_result("all")
 			else:
 				#if ticket_id is given, try to obtain ticket perms
 				if ticket_id:
@@ -38,16 +55,20 @@ def get_rights_for_ticket(user, queue=None, ticket_id=None):
 					#Obtain a registry saved action 
 					#If we dont find the group, we assign queue_dst to None
 					#to see if global rule is created
+					#Try to match if an specific rule is created
 					try:
-						r_obj = models.Rights.objects.get(grp_src=u_group,queue_dst=u_dst_group)
-					except:
-						r_obj = models.Rights.objects.get(grp_src=u_group,queue_dst=None)
-					#Set the models propertys
-					self.can_edit = r_obj.can_edit
-					self.can_view = r_obj.can_view
-					self.can_create = r_obj.can_create
-					self.can_delete = r_obj.can_delete
-					self.can_comment = r_obj.can_comment
+						r_obj = models.Rights.objects.get(grp_src=u_group,queue_dst=u_dst_group)					
+						q_result(r_obj)
+					#Also, try to match if a global rule with no queue is defined
+					except models.Rights.DoesNotExist:
+						try:
+							r_obj = models.Rights.objects.get(grp_src=u_group,queue_dst=None)
+							q_result(r_obj)
+						#And in a last way, ensure user to make nothing
+						#if no query is defined at all
+						except models.Rights.DoesNotExist:
+							q_result(None)
+
 	return u_rights_obj()
 
 #queues to be listed in the "/tickets" section
