f51515c7abb5c37ff7e97c25ede283487afc6ff0
Ramiro <ramiro@gnubit.com>
2016-08-26 03:53:19 +0200

    -Added some snippets to paginate and a usefull (thanks @groutxo !) function to pass multiple arguments to a query

---
+++ core/util.py
@@ -0,0 +1,50 @@
+from django import template
+from django.conf import settings
+from django.core.exceptions import FieldError
+
+register = template.Library()
+
+@register.filter
+def abssub(value, arg):
+    try:
+        return abs(value - arg)
+    except:
+        return 'error'
+
+
+def paginate(queryset, page, limit=settings.PAGINATE_BY):
+    start = (page - 1) * limit
+    end = (page * limit)
+    page_count = queryset.count() / limit
+    queryset = queryset[start:end]
+    return queryset, {
+        'page': page,
+        'page_count': page_count,
+        'last_page': page_count + 1,
+        'pages': xrange(1, page_count + 2),
+    }
+
+
+
+def query_view(model, params, order_by='-id', limit=settings.PAGINATE_BY,**kwargs):
+    params = {key: value for key, value in params.iteritems() if value}
+    params.update({key: value for key, value in kwargs.iteritems() if value})
+
+    try:
+        page = int(params.pop('page', 1))
+    except ValueError:
+        page = 1
+
+    try:
+        queryset = model.objects.filter(**params).order_by(order_by)
+    except FieldError:
+        return {}
+
+    data, pagination = paginate(queryset, page, limit)
+
+    return {
+        'data': data,
+        'pagination': pagination,
+        'query': '&'.join(['%s=%s' % (key, value)
+                           for key, value in params.iteritems()])
+}
