7f0a03c5e76fb7fbe667a83aa89a817a298ffc49
Ramiro <ramiro@gnubit.com>
2016-10-21 03:32:09 +0200

    -In progress little prototype for new dashboard, with open & in progress tickets plus RSS feed -Added some notes to views.py to remember what is needed to render a correct dashboard (will be removed soon)

--- core/views.py
+++ core/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 #from core import views_utils as utils
+from core.models import Ticket
 
 # Create your views here.
 
 @login_required
 def index(request):
+	#1-Test user
+	#2-If admin
+		#3-Obtain 10 last open tickets
+		#4-Obtain 10 last in progress tickets
+	#2-If user
+		#3-Obtain 10 last open assigned tickets
+		#4-Obtain 10 last in progress assigned tickets
+	#3-Render RSS saved on settings
 	return render(request, 'dashboard/dashboard.html')
 
 @login_required
