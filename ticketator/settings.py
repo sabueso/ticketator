4f1e647aad53bf684173520e9ebb9fe7948a1785
watollop <jordillopez@gmail.com>
2017-01-23 17:50:22 +0100

    Added my tickets and bug fixing

--- ticketator/settings.py
+++ ticketator/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django_extensions',
+    'django_extensions',
     'core',
 ]
 
