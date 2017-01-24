690b24a8ad588b3452b1aa87b2aacc75613204ae
Jordi <jordillopez@gmail.com>
2017-01-24 22:20:39 +0100

    Typo

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,8 +2,8 @@ from core.models import Company
 from django.conf import settings
 
 def logo_company(request):
-    Comanpies = Company.objects.all().count()
-    if Comanpies > 0:
+    companies = Company.objects.all().count()
+    if companies > 0:
         logo = settings.MEDIA_ROOT + Company.objects.latest('id').logo.url
     else:
         logo = settings.STATIC_URL + "media/logo/tk-tiny.png"
