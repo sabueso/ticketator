d62b23389aa1f0e4064a8168ee249d5000d21ca9
Jordi <jordillopez@gmail.com>
2017-01-24 22:15:19 +0100

    Fix company logo when there is no companies

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,7 +2,8 @@ from core.models import Company
 from django.conf import settings
 
 def logo_company(request):
-    if Company.objects.latest('id'):
+    Comanpies = Company.objects.all().count()
+    if Comanpies > 0:
         logo = settings.MEDIA_ROOT + Company.objects.latest('id').logo.url
     else:
         logo = settings.STATIC_URL + "media/logo/tk-tiny.png"
