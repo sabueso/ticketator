8a5e9b4e90b1eb71d3caf8a0ff3ea708c0faae9e
pasqual <pasqual.guerrero@immfly.com>
2017-01-25 10:25:42 +0100

    Apply PEP8 on core/admin, core/models, core/templatetags

--- core/admin.py
+++ core/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import *
+from .models import Company, Queue, State, Priority, User, Ticket
+from .models import Rights, UserType
 
-admin.site.register(Company) 
+admin.site.register(Company)
 admin.site.register(Queue)
 admin.site.register(State)
 admin.site.register(Priority)
@@ -15,15 +16,22 @@ admin.site.register(User, UserAdmin)
 
 
 class RightsAdmin(admin.ModelAdmin):
-	list_display = ('id','grp_src','queue_dst','can_view','can_create','can_delete','can_edit','can_comment','enabled')
-	#search_fields = ('subject','body')
+    list_display = (
+        'id', 'grp_src', 'queue_dst', 'can_view', 'can_create', 'can_delete',
+        'can_edit', 'can_comment', 'enabled',
+    )
 
-admin.site.register(Rights,RightsAdmin)
+    #search_fields = ('subject','body')
+
+admin.site.register(Rights, RightsAdmin)
+
 
 class TicketAdmin(admin.ModelAdmin):
-	list_display = ('id','date','create_user','subject','assigned_state','assigned_queue','assigned_user','assigned_prio')
-	search_fields = ('subject','body')
+    list_display = (
+        'id', 'date', 'create_user', 'subject', 'assigned_state',
+        'assigned_queue', 'assigned_user', 'assigned_prio')
+    search_fields = ('subject', 'body')
 
-admin.site.register(Ticket,TicketAdmin)
+admin.site.register(Ticket, TicketAdmin)
 
 admin.site.register(UserType)
