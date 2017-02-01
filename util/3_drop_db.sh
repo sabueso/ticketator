d1357358711f38bd037592055bfc32b76e208771
Ramiro <ramiro@gnubit.com>
2017-02-01 19:46:56 +0100

    Revert "Removed migrations drop"
    This reverts commit 26bf690cb37a8f5d7ea6b3724fd2cb87cdf8bdb7.

--- util/3_drop_db.sh
+++ util/3_drop_db.sh
@@ -1,5 +1,5 @@
 dropdb tickets_db
 dropuser ticket_user_db
 echo "Borrando las migraciones...(root password)"
-#su - -c "rm /home/django/ticketator/core/migrations/00*"
+su - -c "rm /home/django/ticketator/core/migrations/00*"
 
