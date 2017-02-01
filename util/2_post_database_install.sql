74f2793bd913553772b6adb9116d04dd1aebd426
Ramiro <ramiro@gnubit.com>
2017-02-01 18:55:51 +0100

    Added SQL file for post_database_install

---
+++ util/2_post_database_install.sql
@@ -0,0 +1,5 @@
+/* States */
+INSERT INTO core_state VALUES (1, 'open', 'open', false, 'ff6f6f');
+INSERT INTO core_state VALUES (2, 'in progress', 'in progress', false, 'fff42a');
+INSERT INTO core_state VALUES (3, 'closed', 'closed',  false, '85ce55');
+
