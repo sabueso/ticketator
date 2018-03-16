82e419dd59c9daf192ca85e65299499eedd248aa
pasqualguerrero <pasqual.guerrero@gmail.com>
2018-03-16 20:43:18 +0100

    Fixtures no longer exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,5 +14,4 @@ set -e
 
 ./ticketatorapp/manage.py migrate --noinput
 ./ticketatorapp/manage.py collectstatic --noinput
-./ticketatorapp/manage.py loaddata ticketatorapp/fixtures/*.json
 ./ticketatorapp/manage.py runserver "0.0.0.0:${TICKETATOR_PORT}"
