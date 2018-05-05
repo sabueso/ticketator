905842ae2920faf80eb61a6b7ff86974f36c42e8
pasq <pasqual.guerrero@gmail.com>
2018-05-05 19:07:33 +0200

    Remove unused settings (#86)
    Remove to-do file

--- ticketatorapp/ticketator/urls.py
+++ ticketatorapp/ticketator/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
 
 #  Add url to serve media elements on development
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
